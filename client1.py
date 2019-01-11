b3f0e506565741253d1c9c6923a68e67ab71fd4f
star cat <44723961+starcat520@users.noreply.github.com>
2019-01-11 20:06:26 +0800

    Update client1.py

--- client1.py
+++ client1.py
@@ -16,10 +16,31 @@ def encrypt(key, s):
         c[j+1] = c2
         j = j+2
     return c.decode("gbk")
+def decrypt(key, s):
+    c = bytearray(str(s).encode("gbk"))
+    n = len(c) # 计算 b 的字节数
+    if n % 2 != 0 :
+        return ""
+    n = n // 2
+    b = bytearray(n)
+    j = 0
+    for i in range(0, n):
+        c1 = c[j]
+        c2 = c[j+1]
+        j = j+2
+        c1 = c1 - 65
+        c2 = c2 - 65
+        b2 = c2*16 + c1
+        b1 = b2^ key
+        b[i]= b1
+    try:
+        return b.decode("gbk")
+    except:
+        return "failed"
 key = 12
-r = requests.get('http://服务器地址:服务器端口/userrd',data={'u':encrypt(key, 'test'),'p':encrypt(key, 'test'),'o':'1'})
+r = requests.get('http://127.0.0.1:8081/userrd',data={'u':encrypt(key, 'test'),'p':encrypt(key, 'test'),'o':'1'})
 print r.text
-if eval(r.text)["status"] == "true":
-    print("此地调用功能")
+if "true" in decrypt(key,eval(r.text)["status"]):
+    print("校验成功 此地调用功能")
 else:
     print("校验失败 使用调用失败的函数")
